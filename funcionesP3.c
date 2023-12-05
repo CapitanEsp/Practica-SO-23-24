316796da2cea4a6d9d60f261d0f5273cefded57a
aaron <aaron.eiroa@udc.es>
2023-12-05 20:46:45 +0100

    arreglau el jobs

--- funcionesP3.c
+++ funcionesP3.c
@@ -7,127 +7,125 @@
 //FUNCIONES PRINCIPALES (AARON HAGO LO QUE PUEDO)
 //-------------------------------------------------------------------------------------------
 
-void uid(char **command, int nargs){
-    if(nargs < 3){
+void uid(char **command, int nargs) {
+    if (nargs < 3) {
         printf("Credencial real: %d, (%s)\n", getuid(), getUser(getuid()));
         printf("Credencial efectiva: %d, (%s)\n", geteuid(), getUser(geteuid()));
-    }else{
-        if(strcmp(command[1], "-set") == 0){
+    } else {
+        if (strcmp(command[1], "-set") == 0) {
             uid_t newuid;
-            if(strcmp(command[2], "-l") == 0){
-                if(nargs == 4){
+            if (strcmp(command[2], "-l") == 0) {
+                if (nargs == 4) {
                     newuid = getMyuid(command[3]);
-                    if(newuid == -1){
+                    if (newuid == -1) {
                         printf("Usuario no existente %s", command[3]);
-                    }else{
-                        if(seteuid(newuid) != 0){
+                    } else {
+                        if (seteuid(newuid) != 0) {
                             perror("Usuario no existente: ");
                         }
                     }
 
-                }else{
+                } else {
                     printf("Credencial real: %d, (%s)\n", getuid(), getUser(getuid()));
-                    printf("Credencial efectiva: %d, (%s)\n", geteuid(), getUser(geteuid()));                    
+                    printf("Credencial efectiva: %d, (%s)\n", geteuid(), getUser(geteuid()));
                 }
-            }else{
+            } else {
                 newuid = atoi(command[2]);
-                if(seteuid(newuid) != 0){
+                if (seteuid(newuid) != 0) {
                     perror("Imposible cambiar credencial: ");
-                }                
+                }
             }
         }
     }
 }
 
-void showvar(char ** command, int nargs, char ** arg3){
-    if(nargs == 1){
+void showvar(char **command, int nargs, char **arg3) {
+    if (nargs == 1) {
         enviroment(arg3, "arg3 main");
-    }else{
+    } else {
         int posInEnviro = BuscarVariable(command[1], arg3);
-        if(posInEnviro != -1) {
+        if (posInEnviro != -1) {
             printf("Con arg3 main %s(%p) @%p\n", arg3[posInEnviro], arg3[posInEnviro], arg3);
             printf("Con environ %s(%p) @%p\n", varEnviroment[posInEnviro], varEnviroment[posInEnviro], varEnviroment);
             printf("Con getenv %s(%p)\n", getenv(command[1]), getenv(command[1]));
-        }
-        else{
+        } else {
             printf("No ejecutado: %s\n", strerror(errno));
         }
     }
 }
 
-void changevar(char ** command,int nargs, char** arg3){
-    if(nargs == 4){
-        if(strcmp(command[1], "-a") == 0){
-            if(CambiarVariable(command[2], command[3], arg3) == -1){
+void changevar(char **command, int nargs, char **arg3) {
+    if (nargs == 4) {
+        if (strcmp(command[1], "-a") == 0) {
+            if (CambiarVariable(command[2], command[3], arg3) == -1) {
                 printf("Imposible cambiar variable: %s\n", strerror(errno));
             }
-        }else if(strcmp(command[1], "-e") == 0){
-            if(CambiarVariable(command[2], command[3], varEnviroment) == -1){
+        } else if (strcmp(command[1], "-e") == 0) {
+            if (CambiarVariable(command[2], command[3], varEnviroment) == -1) {
                 printf("Imposible cambiar variable: %s\n", strerror(errno));
             }
-        }else if(strcmp(command[1], "-p") == 0){
+        } else if (strcmp(command[1], "-p") == 0) {
             char newVar[500] = "";
             strcpy(newVar, command[2]);
             strcat(newVar, "=");
             strcat(newVar, command[3]);
-            if(putenv(newVar) != 0){
+            if (putenv(newVar) != 0) {
                 printf("Imposible cambiar variable: %s\n", strerror(errno));
             }
-        }else{
+        } else {
             perror("Formato de parametros incorrecto");
         }
-    }else{
+    } else {
         perror("Numero de parametros incorrecto");
     }
 }
 
-void subsvar(char ** command,int nargs, char** arg3){
-    if(nargs == 5){
-        if(strcmp(command[1], "-a") == 0){
-            if(CambiarVariablebyAntonetiiii(command[2], command[3], command[4], arg3) == -1){
+void subsvar(char **command, int nargs, char **arg3) {
+    if (nargs == 5) {
+        if (strcmp(command[1], "-a") == 0) {
+            if (CambiarVariablebyAntonetiiii(command[2], command[3], command[4], arg3) == -1) {
                 printf("Imposible cambiar variable: %s\n", strerror(errno));
             }
-        }else if(strcmp(command[1], "-e") == 0){
-            if(CambiarVariablebyAntonetiiii(command[2], command[3], command[4], varEnviroment) == -1){
+        } else if (strcmp(command[1], "-e") == 0) {
+            if (CambiarVariablebyAntonetiiii(command[2], command[3], command[4], varEnviroment) == -1) {
                 printf("Imposible cambiar variable: %s\n", strerror(errno));
             }
-        }else{
+        } else {
             perror("Formato de parametros incorrecto");
         }
-    }else{
-         perror("Numero de parametros incorrecto");               
+    } else {
+        perror("Numero de parametros incorrecto");
     }
 }
 
-void showenv(char ** command,int nargs, char** arg3){
-    if(nargs < 3){
-        if(nargs == 1){
+void showenv(char **command, int nargs, char **arg3) {
+    if (nargs < 3) {
+        if (nargs == 1) {
             enviroment(arg3, "arg3 main");
-        }else{
-            if(strcmp(command[1], "-environ") == 0){
+        } else {
+            if (strcmp(command[1], "-environ") == 0) {
                 enviroment(varEnviroment, "Enviroment");
             }
-            if(strcmp(command[1], "-addr") == 0){
+            if (strcmp(command[1], "-addr") == 0) {
                 printf("environ:   %p (almacenado en %p)\n", varEnviroment, &varEnviroment);
                 printf("main arg3: %p (almacenado en %p)\n", arg3, &arg3);
             }
         }
-    }else{
-        perror("Numero de parametros incorrecto"); 
+    } else {
+        perror("Numero de parametros incorrecto");
     }
 }
 
-void my_Fork(char ** command,int nargs){
-    if(nargs == 1){
+void my_Fork(char **command, int nargs) {
+    if (nargs == 1) {
         int pidHijo;
-        if((pidHijo = fork()) == 0){
+        if ((pidHijo = fork()) == 0) {
             printf("Ejecutando proceso %d\n", getpid());
-        }
-        else if (pidHijo!=-1){
+        } else if (pidHijo != -1) {
             waitpid(pidHijo, PNULL, 0);
         }
-    }else{
-        perror("Numero de parametros incorrecto"); 
+    } else {
+        perror("Numero de parametros incorrecto");
     }
 }
 
@@ -162,113 +160,117 @@ void exec(char **args, int nargs) { //Execv, execve, execl
     execv(comando, arguments);
 }
 
-
-void jobs(char ** command,int nargs, tListP *L){
-    tPosP p;
-    tItemP data;
-    int aux;
-    pid_t pid;
-    for(p = *L; p != PNULL; p = nextP(p, *L)) {
-        data = getDataP(p, *L);
-        if ((strcmp(data.nsignal, "TERMINADO") != 0) && (strcmp(data.nsignal, "SENALADO") != 0)) {
-            data.prio = getpriority(data.prio, data.pid);
-            aux = waitpid(data.pid, &pid, WNOHANG | WCONTINUED | WUNTRACED);
-            if (aux != -1) {
-                if(aux==data.pid){
+void print_job(tItemP *data) {
+    int aux, pid;
+    if ((strcmp(data->nsignal, "TERMINADO") != 0) && (strcmp(data->nsignal, "SENALADO") != 0)) {
+        data->prio = getpriority(data->prio, data->pid);
+        aux = waitpid(data->pid, &pid, WNOHANG | WCONTINUED | WUNTRACED);
+        if (aux != -1) {
+            if (aux == data->pid) {
                 if (WIFSIGNALED(pid)) {
-                    strcpy(data.nsignal, "SENALADO");
-                    data.signal = WTERMSIG(pid);
+                    strcpy(data->nsignal, "SENALADO");
+                    data->signal = WTERMSIG(pid);
                 } else {
                     if (WIFEXITED(pid)) {
-                        strcpy(data.nsignal, "TERMINADO");
-                        data.signal = WEXITSTATUS(pid);
+                        strcpy(data->nsignal, "TERMINADO");
+                        data->signal = WEXITSTATUS(pid);
                     } else if (WIFSTOPPED(pid)) {
-                        strcpy(data.nsignal, "PARADO");
-                        data.signal = WSTOPSIG(pid);
+                        strcpy(data->nsignal, "PARADO");
+                        data->signal = WSTOPSIG(pid);
                     }
                 }
             }
         }
-        p->data = data;
     }
-        printf("%d\t%s p=%d %s  %s", data.pid, data.user, data.prio, data.launch, /*data.sig.name, data.sig.num,*/ data.comandName);
-        printf("%d %s\n",data.signal,data.nsignal);
+    printf("%d\t%s p=%d %s  %s", data->pid, data->user, data->prio, data->launch, /*data->sig.name, data->sig.num,*/
+           data->comandName);
+    printf("%d %s\n", data->signal, data->nsignal);
+}
+
+void jobs(char **command, int nargs, tListP *L) {
+    tPosP p;
+    tItemP data;
+    for (p = *L; p != PNULL; p = nextP(p, *L)) {
+        data = getDataP(p, *L);
+        print_job(&data);
+        p->data = data;
     }
 }
 
-void deljobs(char ** command, int nargs, tListP *L){
-    if(nargs == 2){
+void deljobs(char **command, int nargs, tListP *L) {
+    if (nargs == 2) {
         tPosP p;
         tItemP i;
-        if(strcmp(command[1], "-term") == 0){
-            for(p = firstP(*L); p != PNULL; p = nextP(p, *L)){
+        if (strcmp(command[1], "-term") == 0) {
+            for (p = firstP(*L); p != PNULL; p = nextP(p, *L)) {
                 i = getDataP(p, *L);
-                if(strcmp("TERMINADO", i.nsignal) == 0){
+                if (strcmp("TERMINADO", i.nsignal) == 0) {
                     deleteAtPosP(p, L);
                 }
             }
         }
-        if(strcmp(command[1], "-sig") == 0){
-            for(p = firstP(*L); p != PNULL; p = nextP(p, *L)){
+        if (strcmp(command[1], "-sig") == 0) {
+            for (p = firstP(*L); p != PNULL; p = nextP(p, *L)) {
                 i = getDataP(p, *L);
-                if(strcmp("SENALADO", i.nsignal) == 0){
+                if (strcmp("SENALADO", i.nsignal) == 0) {
                     deleteAtPosP(p, L);
                 }
             }
-        }     
-    }else{
-        perror("Numero de parametros incorrecto"); 
+        }
+    } else {
+        perror("Numero de parametros incorrecto");
     }
 }
 
-void job(char ** command, int nargs, tListP *L){
-    if(nargs < 2){
+void job(char **command, int nargs, tListP *L) {
+    if (nargs < 2) {
         jobs(command, nargs, L);
-    }else{
-        if(nargs > 3){
+    } else {
+        if (nargs > 3) {
             perror("Numero de parametros incorrecto");
-        }else{
+        } else {
             tPosP p;
             tItemP i;
             int aux;
-            if(nargs == 2){
-                if(atoi(command[1]) !=  getpid()){
+            if (nargs == 2) {
+                if (atoi(command[1]) != getpid()) {
                     p = findDataP(atoi(command[1]), *L);
-                    if(p!=PNULL){
+                    if (p != PNULL) {
                         i = getDataP(p, *L);
-                        printf("Hola%d\t%s p=%d %s %s (%d) %s", i.pid, i.user, i.prio, i.launch, i.nsignal, i.signal, i.comandName);
+                        print_job(&i);
+                        p->data = i;
+                    } else {
+                        jobs(command, nargs, L);
                     }
-                }else{  
-                    jobs(command, nargs, L);
                 }
-            }else{
-                if(strcmp(command[1], "-fg") == 0){
+            } else {
+                if (strcmp(command[1], "-fg") == 0) {
                     p = findDataP(atoi(command[2]), *L);
                     i = getDataP(p, *L);
                     aux = atoi(command[2]);
-                    if(wait(&aux) != -1){
-                        if(WIFEXITED(i.signal)){
-                            printf("Proceso %d terminado normalmente. Valor devuelto: %d\n", atoi(command[1]), WEXITSTATUS(i.signal));
+                    if (wait(&aux) != -1) {
+                        if (WIFEXITED(i.signal)) {
+                            printf("Proceso %d terminado normalmente. Valor devuelto: %d\n", atoi(command[1]),
+                                   WEXITSTATUS(i.signal));
                             //i.sig.name = "TERMINADO";
-                    }
-                    else if(WIFSIGNALED(i.signal)){
-                        printf("Proceso %d terminado por la señal %d", atoi(command[1]), WTERMSIG(i.signal));
-                        //i.sig.name = "SENALADO";
+                        } else if (WIFSIGNALED(i.signal)) {
+                            printf("Proceso %d terminado por la señal %d", atoi(command[1]), WTERMSIG(i.signal));
+                            //i.sig.name = "SENALADO";
                         }
                     }
                 }
             }
-        }        
+        }
     }
 }
 
 void Random(char **args, int nargs, tListP *L) {
     pid_t pid;
     pid = fork(); // Crea un hijo
-    bool background= false;
-    if(args[nargs-1][0]=='&'){
+    bool background = false;
+    if (args[nargs - 1][0] == '&') {
         background = true;
-        args[nargs-1] = NULL;
+        args[nargs - 1] = NULL;
     }
     if (pid == 0) {
         execv(Ejecutable(args[0]), args);
@@ -293,112 +295,111 @@ void Random(char **args, int nargs, tListP *L) {
 //-------------------------------------------------------------------------------------------
 
 //Aaron eres el fucking MVP
-char * getUser(uid_t uid){
+char *getUser(uid_t uid) {
     struct passwd *contraseñaNombre;
     contraseñaNombre = getpwuid(uid);
     return contraseñaNombre->pw_name;
 }
 
-uid_t getMyuid(char * name){
+uid_t getMyuid(char *name) {
     struct passwd *contraseñaNombre;
     contraseñaNombre = getpwnam(name);
-    if(contraseñaNombre != NULL){
+    if (contraseñaNombre != NULL) {
         return contraseñaNombre->pw_uid;
-    }else{
+    } else {
         return -1;
     }
 }
 
-void enviroment(char **varEnviroment, char *enviromentName){
+void enviroment(char **varEnviroment, char *enviromentName) {
     int p = 0;
-    for(;varEnviroment[p] != NULL; p++){
+    for (; varEnviroment[p] != NULL; p++) {
         printf("%p -> %s [%d] = (%p) %s\n", &varEnviroment[p], enviromentName, p, varEnviroment[p], varEnviroment[p]);
     }
 }
 
-int CambiarVariablebyAntonetiiii(char * var1, char *var2, char * valor, char *e[]){
+int CambiarVariablebyAntonetiiii(char *var1, char *var2, char *valor, char *e[]) {
     /*cambia una variable en el entorno que se le pasa como parámetro*/
     /*lo hace directamente, no usa putenv*/
     int pos;
     char *aux;
 
-    if ((pos=BuscarVariable(var1,e))==-1)
-        return(-1);
+    if ((pos = BuscarVariable(var1, e)) == -1)
+        return (-1);
 
-    if ((aux=(char *)malloc(strlen(var2)+strlen(valor)+2))==NULL)
+    if ((aux = (char *) malloc(strlen(var2) + strlen(valor) + 2)) == NULL)
         return -1;
-    strcpy(aux,var2);
-    strcat(aux,"=");
-    strcat(aux,valor);
-    e[pos]= aux;
+    strcpy(aux, var2);
+    strcat(aux, "=");
+    strcat(aux, valor);
+    e[pos] = aux;
     return (pos);
 }
 //-------------------------------------------------------------------------------------------
 //CODIGO DE AYUDA
 //-------------------------------------------------------------------------------------------
 
-void Cmd_fork (char *tr[]){
+void Cmd_fork(char *tr[]) {
     pid_t pid;
 
-    if ((pid=fork())==0){
+    if ((pid = fork()) == 0) {
 /*		VaciarListaProcesos(&LP); Depende de la implementación de cada uno*/
-        printf ("ejecutando proceso %d\n", getpid());
-    }
-    else if (pid!=-1)
-        waitpid (pid,NULL,0);
+        printf("ejecutando proceso %d\n", getpid());
+    } else if (pid != -1)
+        waitpid(pid, NULL, 0);
 }
 
-int BuscarVariable (char * var, char *e[])  /*busca una variable en el entorno que se le pasa como parámetro*/{
-    int pos=0;
+int BuscarVariable(char *var, char *e[])  /*busca una variable en el entorno que se le pasa como parámetro*/{
+    int pos = 0;
     char aux[500];
 
-    strcpy (aux,var);
-    strcat (aux,"=");
+    strcpy(aux, var);
+    strcat(aux, "=");
 
-    while (e[pos]!=NULL)
-        if (!strncmp(e[pos],aux,strlen(aux)))
+    while (e[pos] != NULL)
+        if (!strncmp(e[pos], aux, strlen(aux)))
             return (pos);
         else
             pos++;
-    errno=ENOENT;   /*no hay tal variable*/
-    return(-1);
+    errno = ENOENT;   /*no hay tal variable*/
+    return (-1);
 }
 
 
-int CambiarVariable(char * var, char * valor, char *e[]){
+int CambiarVariable(char *var, char *valor, char *e[]) {
     /*cambia una variable en el entorno que se le pasa como parámetro*/
     /*lo hace directamente, no usa putenv*/
     int pos;
     char *aux;
 
-    if ((pos=BuscarVariable(var,e))==-1)
-        return(-1);
+    if ((pos = BuscarVariable(var, e)) == -1)
+        return (-1);
 
-    if ((aux=(char *)malloc(strlen(var)+strlen(valor)+2))==NULL)
+    if ((aux = (char *) malloc(strlen(var) + strlen(valor) + 2)) == NULL)
         return -1;
-    strcpy(aux,var);
-    strcat(aux,"=");
-    strcat(aux,valor);
-    e[pos]=aux;
+    strcpy(aux, var);
+    strcat(aux, "=");
+    strcat(aux, valor);
+    e[pos] = aux;
 
     return (pos);
 }
 
-int ValorSenal(char * sen){  /*devuelve el numero de senial a partir del nombre*/
+int ValorSenal(char *sen) {  /*devuelve el numero de senial a partir del nombre*/
 
     int i;
-    for (i=0; sigstrnum[i].name!=NULL; i++)
-        if(!strcmp(sen, sigstrnum[i].name))
+    for (i = 0; sigstrnum[i].name != NULL; i++)
+        if (!strcmp(sen, sigstrnum[i].name))
             return sigstrnum[i].num;
     return -1;
 }
 
 
-char *NombreSenal(int sen){  /*devuelve el nombre senal a partir de la senal*/
-			/* para sitios donde no hay sig2str*/
+char *NombreSenal(int sen) {  /*devuelve el nombre senal a partir de la senal*/
+    /* para sitios donde no hay sig2str*/
     int i;
-    for (i=0; sigstrnum[i].name!=NULL; i++)
-        if (sen==sigstrnum[i].num)
+    for (i = 0; sigstrnum[i].name != NULL; i++)
+        if (sen == sigstrnum[i].num)
             return sigstrnum[i].name;
     return ("SIGUNKNOWN");
 }
